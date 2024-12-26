0a4c314d985a025afa3394a3636de0d9f17e5e35
harsh <harshgupt100@gmail.com>
2024-12-26 12:45:35 +0530

    form

--- form/src/Frm.jsx
+++ form/src/Frm.jsx
@@ -1,38 +1,77 @@
 import { useState } from "react"
 
-const Frm = ()=>
+// const Frm = ()=>
+// {
+//     let [h, setH] = useState('')
+//     let [l, setL] = useState('')
+//     let [sb, setSb] = useState(false)
+
+//     function hinput(event)
+//     {
+//         console.log(event.target.value);
+//         setH(event.target.value)
+//     }
+
+//     function linput(event)
+//     {
+//         console.log(event.target.value)
+//         setL(event.target.value)
+//     }
+
+//     function submit()
+//     {
+//         setSb(true)
+//     }
+
+//     return (
+//         <>
+//             <h1>my name is {sb ?  h : ''} </h1>
+//             <input type="text" onChange={hinput} />
+//             <h2>live in {sb ? l : ''} </h2>
+//             <input type="text" onChange={linput} />
+//             <br /><br />
+//             <button onClick={submit}>Submit</button>
+//         </>
+//     )
+// }
+
+
+
+const Frm = () =>
 {
-    let [h, setH] = useState('')
-    let [l, setL] = useState('')
-    let [sb, setSb] = useState(false)
+    let [inputname, setinput] = useState({
+        username:'',
+        address:''
+    })
 
-    function hinput(event)
+    const hinput = (event) =>
     {
-        console.log(event.target.value);
-        setH(event.target.value)
+        const {name,value} = event.target;
+        setinput({
+            ...inputname,
+            [name]:value
+        })
     }
-
-    function linput(event)
-    {
-        console.log(event.target.value)
-        setL(event.target.value)
-    }
-
-    function sbb()
-    {
-        setSb(true)
-    }
-
-    return (
-        <>
-            <h1>my name is {sb ?  h : ''} </h1>
-            <input type="text" onChange={hinput} />
-            <h2>live in {l} </h2>
-            <input type="text" onChange={linput} />
-
-            <button onClick={sbb}>Submit</button>
-        </>
-    )
+        const finalsubmit = (event) =>
+        {
+            event.preventDefault();
+            console.log(inputname)
+        }
+        return(
+            <>
+                <form onSubmit={finalsubmit}>
+                    <label htmlFor="">Name</label>
+                    <input type="text" name = "username" value = {inputname.username} onChange = {hinput} />
+                    <br /> <br />
+                    <label htmlFor="" >Address</label>
+                    <input type="text" name="address" value={inputname.address} onChange = {hinput} />
+                    <br /><br />
+                    <input type="submit" />
+                </form>
+            </>
+        ) 
 }
 
+//spread operator rest opera
+// tor
 export default Frm
