1b8d6ad4314db5421aaf1086c78d102ac37603e9
harsh <harshgupt100@gmail.com>
2025-01-20 12:32:47 +0530

    redux

---
+++ Redux/src/App.jsx
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* SLice                            / store->glodbal
+        ->name
+        ->initial
+        ->reducer:
+        {
+
+        } */}
+
+        
+
+
+    </>
+  )
+}
+
+export default App
---
+++ Redux/src/Createslice.jsx
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counter = createSlice({
+    name:"counter",
+    initialState:{value:0},
+    reducers:{
+        inc:(state)=>{state.value+=1},
+        inc:(state)=>{state.value-=1}
+    }
+})
+
+export const {inc,dec} = counter.actions
+export default counter.reducer
